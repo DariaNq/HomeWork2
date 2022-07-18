298a9dcf15d46f93e4c11f1cde8ea138ce137df2
Darina <darina.vaxrusheva@bk.ru>
2022-07-18 13:35:36 +0300

    homework

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// Console.WriteLine("введите число A");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите число B");
+// int b = Convert.ToInt32(Console.ReadLine());
+// int x = a;
+
+// for (int i = 1; i < b; i++)
+// {
+// x = x * a;
+// }
+// Console.WriteLine(a + " в степени " + b + " равно: " + x);
+
+//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// Console.WriteLine("введите число");
+// int i = Convert.ToInt32(Console.ReadLine());
+// int sum = 0;
+
+// // while (i > 0)
+// // {
+// // int num = i % 10;
+// // i = i / 10;
+// // sum = sum + num;
+// // }
+// // Console.WriteLine("сумма всех цифр в числе равна: " + sum);
+
+
+// //Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// int GenerateRandomArray(int N, int start, int end)
+// {
+//     int[] RandomArray = new int [N];
+//     for (int i = 0; i < N; i++)
+//     {
+//         RandomArray[i] = new Random().Next(start, end+1);
+//     }
+//     return RandomArray;
+// }
+// void ShowArray (int[] array)
+// {
+//     for (int i = 0; i < array.Length; i ++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+//     Console.WriteLine();
+// }
+// Console.WriteLine(" ");
+
