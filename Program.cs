6d857adf680129757afbef80987ed5aa9a947ffd
Darina <darina.vaxrusheva@bk.ru>
2022-07-19 23:42:38 +0300

    homework

--- Program.cs
+++ Program.cs
@@ -183,8 +183,109 @@
 
 // Console.WriteLine($"Сумма положительных: {sum_pos}. Сумма отрицательных: {sum_neg}");
 
+//Задача 39. напишите программу,которая перевернет одномерный массив(последний элемент будет на 1ом месте, и наоборот)
+
+// int[] GetArray(int size, int minValue, int maxValue)
+// {
+// int[] res = new int[size];
+
+// for (int i = 0; i < size; i++)
+// {
+// res[i] = new Random().Next(minValue, maxValue + 1);
+// }
+// return res;
+// }
+
+// void ReversArray1(int[] inArray)
+// {
+// for (int i = 0; i < inArray.Length / 2; i++)
+// {
+// int k = inArray[i];
+// inArray[i] = inArray[inArray.Length - i - 1];
+// inArray[inArray.Length - i - 1] = k;
+// }
+// }
+
+// int[] ReversArray2(int[] inArray)
+// {
+// int[] result = new int[inArray.Length];
+// for (int i = 0; i < inArray.Length; i++)
+// {
+// result[i] = inArray[inArray.Length - 1 - i];
+// }
+// return result;
+// }
 
 
 
+//Задача 40: Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины.
+
+// Console.WriteLine("Введите первое число");
+// int A = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число");
+// int B = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите третье число");
+// int C = Convert.ToInt32(Console.ReadLine());
+
+// if (A <= (B+C) && B <= (C+A) && C <= (A+B))
+// {
+//   Console.WriteLine("Такой треугольник может существовать");
+// }
+// else{
+//     Console.WriteLine("Такой треугольник не может существовать");
+// }
+
+
+//Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+
+//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+ 
+// Console.Write($"Введите число М(количество чисел): ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// int[] Numbers1 = new int[m];   
+
+// void InputNumbers(int m){
+// for (int i = 0; i < m; i++)
+//   {
+//     Console.Write($"Введите {i+1} число: ");    //выводит попорядку числа с клавиатуры 
+//     Numbers1[i] = Convert.ToInt32(Console.ReadLine());
+//   }
+// }
+// int quantity(int[] Numbers1)
+// {
+//   int count = 0;
+//   for (int i = 0; i < Numbers1.Length; i++)
+//   {
+//     if(Numbers1[i] > 0 ) count += 1; 
+//   }
+//   return count;
+// }
+
+// InputNumbers(m);
+
+// Console.WriteLine($"Введено чисел больше 0: {quantity(Numbers1)} ");
+
+
+//Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// Console.WriteLine("Введите зечание b1");   // запрашиваем у пользователя 
+// double b1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите значение k1");
+// double k1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите значение b2");
+// double b2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите значение k2");
+// double k2 = Convert.ToInt32(Console.ReadLine());
+
+// if (k1 == k2 && b1 == b2){
+//     Console.WriteLine("Прямые совподают - точек пересечения бесконечное множество");
+// }
+// else {
+//     Console.WriteLine("Координаты точки пересечения прямых при заданных значениях: ");
+//     double x = (b1 -b2)/(k2 - k1);
+//     double y = k1 *  x +b1;
+//     Console.WriteLine($"k1 = {k1}, b1 = {b1}, k2 = {k2}, b2 = {b2} -> ({x}; {y})");
+// }
 
 
