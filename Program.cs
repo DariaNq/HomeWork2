f05a0c4cbf9ef39459d881c4bbebf92b21bf9c94
Darina <darina.vaxrusheva@bk.ru>
2022-07-19 11:04:40 +0300

    homewk

--- Program.cs
+++ Program.cs
@@ -48,3 +48,143 @@
 // }
 // Console.WriteLine(" ");
 
+//Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// int[] numbers = new int[10];
+
+// void FillArray(int[] array, int min, int max){
+//   for (int i = 0; i<array.Length; i++ ){
+//     array[i] = new Random().Next(min, max);
+//   }
+// }
+
+// void WriteArray(int[] array){
+//     for (int i = 0; i<array.Length; i++ ){
+//     Console.Write(array[i] + " ");
+//   }
+//   Console.WriteLine();
+// }
+
+// int QuantityPositive(int[] array){
+//     int quantity = 0;
+//     for (int i = 0; i<array.Length; i++ ){
+//     if (array[i] % 2 == 1)
+//     {
+//       quantity++;
+//     }
+//   }
+//   return quantity;
+// }
+
+// FillArray(numbers, 100, 999);
+// WriteArray(numbers);
+// Console.WriteLine();
+
+// int quantity = QuantityPositive(numbers);
+// Console.WriteLine($"Количество чётных чисел в массиве: {quantity}");
+
+
+
+//Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+// Console.Write($"Введите количество элементов массива: ");
+// int numberElements = Convert.ToInt32(Console.ReadLine()); 
+
+// int RandomNumbers(int numberElements, int min, int max)
+//   {
+//   int[] randomNumbers = new int[numberElements];
+//   int sumElements = 0;
+//   Console.Write("ваш массив : ");
+
+//     for (int i = 0; i <randomNumbers.Length; i++ ){
+//       randomNumbers[i] = new Random().Next(min, max);
+
+//       Console.Write(randomNumbers[i] + " ");
+
+//       if (i % 2 != 1)
+//       {
+//         sumElements = sumElements + randomNumbers[i];
+//       }
+//     }
+//   return sumElements;
+//   }
+
+// int randomNumbers =  RandomNumbers(numberElements, 50, 99);
+
+// Console.WriteLine($"\nСумма элементов, стоящих на нечётных позициях: {randomNumbers}");
+
+//Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+// int[] arrayNumbers = new int [15];
+//   for (int i = 0; i < arrayNumbers.Length; i++ )
+//   {
+//     arrayNumbers[i] = new Random().Next(1, 15);
+//     Console.Write(arrayNumbers[i] + " ");
+//   }
+
+// int maxNumber = arrayNumbers[0];
+// int minNumber = arrayNumbers[0];
+
+//   for (int i = 1; i < arrayNumbers.Length; i++)
+//   {
+//     if (maxNumber < arrayNumbers[i])
+//     {
+//       maxNumber = arrayNumbers[i];
+//     }
+//         if (minNumber > arrayNumbers[i])
+//     {
+//       minNumber = arrayNumbers[i];
+//     }
+//   }
+
+//   int nambers = maxNumber - minNumber;
+
+//   Console.WriteLine($"\nразница между между максимальным ({maxNumber}) и минимальным({minNumber}) элементами: {nambers}");
+
+
+
+
+
+//Задача Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. Найдите сумму отрицательных и положительных элементов массива.
+
+// int[] CreateRandomArray(int size, int min, int max)
+// {
+// int[] array = new int[size];
+// for (int i = 0; i < size; i++)
+// {
+// array[i] = new Random().Next(min, max + 1);
+// }
+// return array;
+// }
+
+// void ShowArray(int[] array)
+// {
+
+// for (int i = 0; i < array.Length; i++)
+// {
+// Console.Write(array[i] + " ");
+// }
+// Console.WriteLine();
+// }
+
+// int[] myArray = CreateRandomArray(12, -9, 9);
+// ShowArray(myArray);
+
+// int sum_pos = 0;
+// int sum_neg = 0;
+
+// for (int i = 0; i < myArray.Length; i++)
+// {
+// if (myArray[i] > 0)
+// sum_pos += myArray[i];
+// else
+// sum_neg += myArray[i];
+// }
+
+// Console.WriteLine($"Сумма положительных: {sum_pos}. Сумма отрицательных: {sum_neg}");
+
+
+
+
+
+
